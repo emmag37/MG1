474ae1e2e8434280a3aa3fa1d63735efa793b69d
emmag37 <emmaglenn1@gmail.com>
2026-02-19 15:15:24 -0500

    successfully spawn a player with a random sprite

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
     void Awake()
     {
         sr = GetComponent<SpriteRenderer>();
+        sr.sortingOrder = 1;
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
--- Assets/Scripts/PlayerGenerator.cs
+++ Assets/Scripts/PlayerGenerator.cs
@@ -2,17 +2,12 @@ using UnityEngine;
 
 public class PlayerGenerator : MonoBehaviour
 {
-    // Outside Objects - inspector
-    public Player player;
+    // Prefabs
+    public GameObject player;
 
     // Sprites - inspector
-    public Sprite color1;
-    public Sprite color2;
-    public Sprite color3;
-    public Sprite color4;
-    public Sprite color5;
-    public Sprite color6;
-    public Sprite wildcard;
+        // 0: color1, 1: color2, 2: color3, 3: color4, 4: color5, 5: color6, 6: wildcard
+    public Sprite[] sprites;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -30,40 +25,15 @@ public class PlayerGenerator : MonoBehaviour
     void spawnPlayer()
     {
         // use a random number to pick the sprite
-        int num = Random.Range(1, 8);
+        int num = Random.Range(0, 7);
         Debug.Log("random number: " + num);
 
-        Sprite color;
-        switch (num)
-        {
-            case 1:
-                color = color1;
-                break;
-            case 2:
-                color = color2;
-                break;
-            case 3:
-                color = color3;
-                break;
-            case 4:
-                color = color4;
-                break;
-            case 5:
-                color = color5;
-                break;
-            case 6:
-                color = color6;
-                break;
-            case 7:
-                color = wildcard;
-                break;
-            default:
-                color = null;
-                break;
-        }
+        Sprite color = sprites[num];
         Debug.Log(color ? "sprite: " + color.name : "No sprite selected");
 
-        // instantiate a new player
+        // instantiate a new player and set its sprite
+        GameObject newPlayer = Instantiate(player, transform.position, transform.rotation);
+        newPlayer.GetComponent<Player>().SetSprite(color);
 
     }
 }
