4ea9c65ca7fdeda47b75f1278fa4c3c1fe197035
emmag37 <emmaglenn1@gmail.com>
2026-04-04 11:28:32 -0500

    implemented score history scroll view

--- Assets/Scripts/Presentation/UI/Views/PopUpViews/ScoreHistoryView.cs
+++ Assets/Scripts/Presentation/UI/Views/PopUpViews/ScoreHistoryView.cs
@@ -1,15 +1,38 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class ScoreHistoryView : PopUpView<IGameData>
 {
+    [SerializeField] private Transform content;
+    [SerializeField] private GameObject listItem;   // prefab
+    
+
     public override void Show(IGameData data)
     {
-        foreach (int score in data.ScoreHistory)
-        {
-            Debug.Log($"score: {score}");
-        }
+        PopulateList(data.ScoreHistory);
 
         base.Show(data);
+    }
+
+    private void PopulateList(IReadOnlyList<int> scoreHistory)
+    {
+        // clear existing list
+        foreach (Transform child in content)
+        {
+            Destroy(child.gameObject);
+        }
+
+        foreach (int score in scoreHistory)
+        {
+            Debug.Log($"score: {score}");
+
+            // create the list item
+            GameObject item = Instantiate(listItem, content);
+
+            // set its text to score
+            Text text = item.GetComponent<Text>();
+            text.text = $"{score}";
+        }
     }
 }
