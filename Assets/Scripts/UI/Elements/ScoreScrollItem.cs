d18e0a4f8ae15dfd6c636397f2caa80698aae810
emmag37 <emmaglenn1@gmail.com>
2026-09-09 14:12:45 -0500

    completed ui polish
    - note: will likely have to add back in a few scripts from my library as you go

--- Assets/Scripts/UI/Elements/ScoreScrollItem.cs
+++ Assets/Scripts/UI/Elements/ScoreScrollItem.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 
 public class ScoreScrollItem : MonoBehaviour, IScrollItem<int>
 {
+    // ==================================================
+    // Public Fields
+    // ==================================================
+    public GameObject GameObject => gameObject;
+
+    // ==================================================
+    // Inspector Fields
+    // ==================================================
     [SerializeField] private Text rankText;
     [SerializeField] private Text scoreText;
 
-    public GameObject GameObject => gameObject;
+
+    // ==================================================
+    // Public Methods
+    // ==================================================
 
     public void Set(int data, int index)
     {
+        if (index + 1 > UIConstants.NumScores)
+            throw new ArgumentOutOfRangeException($"[ScoreScrollItem] Index {index} out of range");
+
         scoreText.text = $"{data}";
         rankText.text = $"{index + 1}";
     }
